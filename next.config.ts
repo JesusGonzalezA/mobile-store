131e4e138d29e008a203068386bb85cddb2a4185
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2025-07-15 00:27:30 +0200

    Try

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import { NextConfig } from "next"
 import createNextIntlPlugin from "next-intl/plugin"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/mobile-store",    
+  images: {
+    unoptimized: true,
+  },
+}
 
 const withNextIntl = createNextIntlPlugin("./src/shared/intl/request.ts")
 export default withNextIntl(nextConfig)
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { notFound } from "next/navigation"
+import { setRequestLocale } from "next-intl/server"
 import { hasLocale, NextIntlClientProvider } from "next-intl"
 import { routing } from "@/shared/intl/routing"
 import { CartIconWrapper } from "@app/cart/components/CartIcon"
@@ -9,6 +10,10 @@ import "@/app/globals.css"
 export const metadata: Metadata = {
 	title: "Mobile Store",
 	description: "Mobile store application. Browse and buy mobile devices.",
+}
+
+export function generateStaticParams() {
+	return routing.locales.map((locale) => ({ locale }));
 }
 
 export default async function RootLayout({
@@ -22,6 +27,9 @@ export default async function RootLayout({
 	if (!hasLocale(routing.locales, locale)) {
 		notFound()
 	}
+
+	setRequestLocale(locale);
+
 	return (
 		<html lang={locale}>
 			<body>
