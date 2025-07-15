3dd9cfc5c9d05ad09664aaeca05618a55d67009a
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2025-07-15 08:33:48 +0200

    Fix format

--- test/app/list/accessibility.test.tsx
+++ test/app/list/accessibility.test.tsx
@@ -1,21 +1,21 @@
 import { describe, it, expect } from "vitest"
 import { waitFor } from "@testing-library/dom"
-import { toHaveNoViolations, axe } from 'jest-axe'
+import { toHaveNoViolations, axe } from "jest-axe"
 import Page from "@app/list/(view)/page"
 import { renderWithProviders } from "../../utils/renderWithProviders"
 expect.extend(toHaveNoViolations)
 
 describe("Accessibility", () => {
-    it("should have no axe errors when the app is rendered", async () => {
-        const { queryByText, intl, container } = renderWithProviders(<Page />, {
-            locale: "en",
-        })
+	it("should have no axe errors when the app is rendered", async () => {
+		const { queryByText, intl, container } = renderWithProviders(<Page />, {
+			locale: "en",
+		})
 
-        await waitFor(() => {
-            const zeroResultsText = intl.list.search.results.replace("{count}", "0")
-            expect(queryByText(zeroResultsText)).toBeNull()
-        })
+		await waitFor(() => {
+			const zeroResultsText = intl.list.search.results.replace("{count}", "0")
+			expect(queryByText(zeroResultsText)).toBeNull()
+		})
 
-        expect(await axe(container)).toHaveNoViolations();
-    })
+		expect(await axe(container)).toHaveNoViolations()
+	})
 })
--- test/architecture/features/list.test.ts
+++ test/architecture/features/list.test.ts
@@ -3,19 +3,24 @@ import * as path from "path"
 import { describe, it, expect } from "vitest"
 
 describe("architecture", () => {
-    describe('list feature', () => {
+	describe("list feature", () => {
+		it("should adhere to diagram", { timeout: 6000 }, async () => {
+			const diagramLocation = path.resolve(
+				"src",
+				"app",
+				"[locale]",
+				"(list)",
+				"architecture.puml",
+			)
 
-    it('should adhere to diagram', { timeout: 6000 }, async () => {
-        const diagramLocation = path.resolve('src', 'app', '[locale]', '(list)', 'architecture.puml')
+			const violations = await slicesOfProject()
+				.definedBy("src/app/[locale]/(list)/(**)/")
+				.should()
+				.ignoringExternalDependencies()
+				.adhereToDiagramInFile(diagramLocation)
+				.check()
 
-        const violations = await slicesOfProject()
-            .definedBy('src/app/[locale]/(list)/(**)/')
-            .should()
-            .ignoringExternalDependencies()
-            .adhereToDiagramInFile(diagramLocation)
-            .check()
-
-            expect(violations).toEqual([])
-    })
-})    
+			expect(violations).toEqual([])
+		})
+	})
 })
