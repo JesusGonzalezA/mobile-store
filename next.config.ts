818b9ddc4af0d6d0cef8ca167e5c465c1793cd5c
Jesús González Álvarez <Jesus.Gonzalez@unit4.com>
2025-07-15 10:28:48 +0200

    Fix format

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import { NextConfig } from "next"
 import createNextIntlPlugin from "next-intl/plugin"
 
-const developmentConfig: NextConfig = {
-}
+const developmentConfig: NextConfig = {}
 const productionConfig: NextConfig = {
 	output: "export",
 	basePath: "/mobile-store",
@@ -14,8 +13,10 @@ const productionConfig: NextConfig = {
 		NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
 		NEXT_PUBLIC_SEARCH_LIMIT: process.env.NEXT_PUBLIC_SEARCH_LIMIT,
 	},
-	compress: true
+	compress: true,
 }
 
 const withNextIntl = createNextIntlPlugin("./src/shared/intl/request.ts")
-export default withNextIntl(process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig)
+export default withNextIntl(
+	process.env.NODE_ENV === "production" ? productionConfig : developmentConfig,
+)
